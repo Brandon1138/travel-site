5aeac238c51da7a2f4c607fd2c1e2d3a3d32e813
Brandon <brandon.aron38@gmail.com>
2022-08-21 19:37:44 +0300

    added header styling

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,11 @@ const postCSSPlugins = [
   require('postcss-mixins'),
   require('postcss-simple-vars'),
   require('postcss-nested'),
+  // If in the future the creator of the postcss-hexrgba package
+  // releases an update (it is version 2.0.1 as I'm writing this)
+  // then it will likely work with PostCSS V8 so you can uncomment
+  // the line below and also install the package with npm.
+  //require('postcss-hexrgba'),
   require('autoprefixer')
 ]
 
@@ -15,12 +20,14 @@ module.exports = {
     path: path.resolve(__dirname, 'app')
   },
   devServer: {
-    static: {
-        directory: path.join(__dirname, 'app'),
+    before: function(app, server) {
+      server._watch('./app/**/*.html')
     },
+    contentBase: path.join(__dirname, 'app'),
     hot: true,
-    port: 3000
-}, 
+    port: 3000,
+    host: '0.0.0.0'
+  },
   mode: 'development',
   module: {
     rules: [
