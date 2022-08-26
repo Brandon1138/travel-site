58e94280af583915c8b6acc72d05cd64cb635fe1
Brandon <brandon.aron38@gmail.com>
2022-08-26 18:29:16 +0300

    added our build process config

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -14,10 +14,10 @@ document.querySelectorAll(".open-modal").forEach(el => {
   el.addEventListener("click", e => {
     e.preventDefault()
     if (typeof modal == "undefined") {
-      import/* webpackChunkName: "modal" */('./modules/Modal').then(x => {
-        modal =  new x.default()
+      import(/* webpackChunkName: "modal" */ './modules/Modal').then(x => {
+        modal = new x.default()
         setTimeout(() => modal.openTheModal(), 20)
-      }).catch(() => console.log("there was a problem."))
+      }).catch(() => console.log("There was a problem."))
     } else {
       modal.openTheModal()
     }
