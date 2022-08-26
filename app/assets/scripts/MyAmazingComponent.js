5f9e1806cccc037eafc5baea7e39b87ea012e3a4
Brandon <brandon.aron38@gmail.com>
2022-08-26 20:34:07 +0300

    added react component

---
+++ app/assets/scripts/MyAmazingComponent.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function MyAmazingComponent() {
+  return (
+    <div>
+      <h1>This is my amazing React Component</h1>
+      <p>React is great, the sky is blue, grass is green.</p>
+    </div>
+  )
+}
+
+export default MyAmazingComponent
